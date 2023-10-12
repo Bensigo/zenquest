5a759f58e68a47e2da4f13fe2166f8d85a6217a0
bensigo <sigocode@gmail.com>
2023-10-12 04:45:11 +0400

    fix: build

--- src/env.mjs
+++ src/env.mjs
@@ -8,7 +8,7 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
-    // DIRECT_DATABASE_URL: z.string().url(),
+    DIRECT_DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET:
       process.env.NODE_ENV === "production"
